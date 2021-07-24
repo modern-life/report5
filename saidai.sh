8d8eeb1fef516d01c0d57a21fc111fcb1af442a3
Ayaka Otsuka <z21073ao@aiit.ac.jp>
2021-07-24 22:57:48 +0900

    test02

--- saidai.sh
+++ saidai.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
- 
-echo "一つ目の数字 "
-read a
-if expr "$a" : "[0-9]*$" >&/dev/null; then
- 
-	echo "二つ目の数字"
-	read  i
-	if expr "$i" : "[0-9]*$" >&/dev/null ;then
-	
-	while ["$i" -ne 0];
 
+
+echo "一つ目の自然数を入れてください"
+read a
+if expr $a : "[0-9]*$" >&/dev/null; then  	
+	echo "二つ目の自然数を入れてください"
+	read  i 
+	if expr $i : "[0-9]*$" >&/dev/null ;then
+  	 	x=$a
+	 	y=$i 
+		while [ $y -ne 0 ];do		
+		keisan=$(( $x % $y ))
+		x=$y
+		y=$keisan
+		  
+	done
+	echo "$aと$iの最大公約数は$xです"
 	else 
-	echo "NG"	
+	echo "自然数を入れてください"	
 	fi			
 else
-echo "NG"
+echo "自然数を入れてください"
 fi
