29fb78372889be60f6422b76d1b5c6b8ed77fa8f
Ayaka Otsuka <z21073ao@aiit.ac.jp>
2021-07-27 00:21:14 +0900

    Test4

--- saidai.sh
+++ saidai.sh
@@ -3,27 +3,32 @@
 echo "一つ目の自然数を入れてください"
 read a
 
-if [ $a -eq 0 ];then
+if  [  $a -eq 0  ] ;then
 echo "0入れないで！"
+
 	elif expr $a : "[0-9]*$" >/dev/null ; then  	
 	echo "二つ目の自然数を入れてください"	
 	read i
-		if [ $i -eq 0 ];then
+		if  [  $i -eq 0  ] ;then
 		echo "0入れないで！"
-	
-
-		elif expr $i : "[0-9]*$" >/dev/null ; then
+		elif expr $i : "[0-9]*$" >/dev/null ;   then
   		
-			x=$a
-			y=$i 
-		while [ $y -ne 0 ];do		
-	
-		keisan=$(( $x ))
-		x=$y
-		y=$keisan
-		  
+		e= $a
+		
+  
+	   	if  [ $i -lt $e ] ;then
+		e= $i
+		fi
+			while [$e -ne 0] do
+			x= `expr $a % $e`
+			y= `expr $i % $e`
+			if  [ $x -eq 0 -a $y -eq 0  ] ;then
+		echo "$aと$iの最大公約数は$eです"
+		break
+
+	fi
+	e= `expr $e - 1`
 	done
-	echo "$aと$iの最大公約数は$xです"
 	else 
 	echo "自然数を入れてください"	
 	exit 1
