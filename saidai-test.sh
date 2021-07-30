b134c7d25f6f570bcceb717b087df9265e1f281d
Ayaka Otsuka <z21073ao@aiit.ac.jp>
2021-07-30 17:15:15 +0900

    TEST

--- saidai-test.sh
+++ saidai-test.sh
@@ -10,7 +10,7 @@ result="/tmp$$-result"
 #一つ目の文字が自然数か判定
 
 #自然数でなかった場合
-echo"自然数を入れてください" > ${ans}
+echo "自然数を入れてください" > ${ans}
 './saidai.sh' i=-1 > ${result}
 diff ${ans} ${result} || exit 1
 #0が入力された場合
