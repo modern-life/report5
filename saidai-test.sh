dc61d03422fbded2ab5a274509a929dce9a48635
Ayaka Otsuka <z21073ao@aiit.ac.jp>
2021-07-30 22:06:56 +0900

    TEST

--- saidai-test.sh
+++ saidai-test.sh
@@ -2,6 +2,7 @@
 
 ans="/tmp/$$-ans"
 result="/tmp/$$-result"
+err="/tmp/$$-error"
 
 
 
@@ -9,27 +10,26 @@ result="/tmp/$$-result"
 
 echo "1つ目に入力した文字に問題があります 自然数を入れてください" > ${ans}
 ./saidai.sh a 10 > ${result}
-diff ${ans} ${result} || exit 1
+diff ${ans} ${result} || echo "error in 1-1"  >> ${err}
 
 #1つ目に入力されたが文字が0だった場合
 
 echo "1つ目に入力した文字に問題があります 0の最大公約数はありません" > ${ans}
 ./saidai.sh 0 9 > ${result}
-diff ${ans} ${result} || exit 1
+diff ${ans} ${result} || echo "error in 1-2"  >> ${err}
 
 
 #2つ目に入力された文字が自然数でなかった場合
 
 echo "2つ目に入力した文字に問題があります 自然数を入れてください" > ${ans}
-
 ./saidai.sh 20 b > ${result}
-diff ${ans} ${result} || exit 1
+diff ${ans} ${result} || echo "error in 2-1"  >> ${err}
 
 #2つ目に入力された文字が0だった場合
 
 echo "2つ目に入力した文字に問題があります 0の最大公約数はありません" > ${ans}
 ./saidai.sh 5 0  > ${result}
-diff ${ans} ${result} || exit 1
+diff ${ans} ${result} || echo "error in 2-2"  >> ${err}
 
 #2つの文字がどちらも自然数だった場合-最大公約数判定
 
@@ -40,6 +40,8 @@ diff ${ans} ${result} || exit 0
 
 
 
-
-rm /tmp/$$-*
+if [ -f ${err} ]; then
+	cat ${err}
+	rm /tmp/$$-*
+	exit 1
 
