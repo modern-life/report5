d66f393fea5c428f50e225ce7e7c981ec235757f
Ayaka Otsuka <z21073ao@aiit.ac.jp>
2021-07-30 18:43:45 +0900

    TEST

--- saidai.sh
+++ saidai.sh
@@ -1,38 +1,34 @@
 #!/bin/bash
 
-echo "一つ目の自然数を入れてください"
-read a
 
-if  [  $a -eq 0  ] ;then
-echo "0の最大公約数はありません"
+if  [  $1 -eq 0  ] ;then
+echo "1つ目に入力した文字に問題があります 0の最大公約数はありません"
 
-	elif expr $a : "[0-9]*$" >/dev/null ; then  	
-	echo "二つ目の自然数を入れてください"	
-	read i
-	if  [  $i -eq 0  ] ;then
-		echo "0の最大公約数はありません"
-		elif expr $i : "[0-9]*$" >/dev/null ;   then
+	elif expr $1 : "[0-9]*$" >/dev/null ; then  	 
+	if  [  $2 -eq 0  ] ;then
+		echo "2つ目に入力した文字に問題があります  0の最大公約数はありません"
+		elif expr $2 : "[0-9]*$" >/dev/null ;   then
 		
-		e=$a
-	   	if  [ $i -lt $e ] ;then
-  		e=$i
-	
+		e=$1
+	   	if  [ $2 -lt $e ] ;then
+  		e=$2
+
 		fi
 			while [ $e -ne  0 ] ;do
-			x=`expr $a % $e`
-			y=`expr $i % $e`
+			x=`expr $1 % $e`
+			y=`expr $2 % $e`
 			if  [ $x -eq 0 -a $y -eq 0  ] ;then
-		echo "$aと$iの最大公約数は$eです"
+		echo "$1と$2の最大公約数は$eです"
 		break
 
 	fi
 	e=`expr $e - 1`
 	done
 	else 
-	echo "自然数を入れてください"	
+	echo "2つ目に入力した文字に問題があります 自然数を入れてください"	
 	exit 1
 	fi	
 else
-	echo "自然数を入れてください"
+	echo "1つ目に入力した文字に問題があります 自然数を入れてください"
 exit 1
 fi
