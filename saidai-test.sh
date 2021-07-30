bd4ef746be90d16549c44273adef4c92de1728a5
Ayaka Otsuka <z21073ao@aiit.ac.jp>
2021-07-30 17:21:36 +0900

    TEST

--- saidai-test.sh
+++ saidai-test.sh
@@ -10,12 +10,14 @@ result="/tmp/$$-result"
 #一つ目の文字が自然数か判定
 
 #自然数でなかった場合
+
 echo "自然数を入れてください" > ${ans}
-'./saidai.sh' i=-1 > ${result}
+./saidai.sh i=-1 > ${result}
 diff ${ans} ${result} || exit 1
 #0が入力された場合
+
 echo "0の最大公約数はありません" > ${ans}
-`./saidai.sh` i=0 >${result}
+./saidai.sh i=0 >${result}
 diff ${ans} ${result} || exit 1
 
 rm /tmp/$$-*
