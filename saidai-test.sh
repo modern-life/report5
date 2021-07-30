f756ab73ede991176d570611ea7f15de8fcbaf72
Ayaka Otsuka <z21073ao@aiit.ac.jp>
2021-07-30 19:12:23 +0900

    TEST

--- saidai-test.sh
+++ saidai-test.sh
@@ -20,10 +20,25 @@ diff ${ans} ${result} || exit 1
 
 #2つ目に入力された文字が自然数でなかった場合
 
+echo "2つ目に入力した文字に問題があります 自然数を入れてください" > ${ans}
+./saidai.sh 20 b > $[result]
+diff $[ans] $[result] || exit 1
+
 #2つ目に入力された文字が0だった場合
 
+echo "2つ目に入力した文字に問題があります 0の最大公約数はありません" > ${ans}
+./saidai.sh 5 0  > ${result}
+diff ${ans} ${result} || exit 1
 
 #2つの文字がどちらも自然数だった場合-最大公約数判定
+
+echo "$1と$2の最大公約数は$eです" > ${ans}
+./saidai/sh 20 30 > ${result}
+diff ${ans} ${result} || exit 0
+
+
+
+
 
 rm /tmp/$$-*
 
