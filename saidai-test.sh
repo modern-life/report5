9588330c9e5aaeedb8b0684cd68a719032f7d263
Ayaka Otsuka <z21073ao@aiit.ac.jp>
2021-07-30 16:51:58 +0900

    TEST

--- saidai-test.sh
+++ saidai-test.sh
@@ -1,11 +1,22 @@
 #!/bin.sh
 
-ans 
+ans="/tmp/$$-ans"
+result="/tmp$$-result"
+
+
+
 #一つ目の文字を入力する
 
 #一つ目の文字が自然数か判定
 
 #自然数でなかった場合
-echo"自然数を入れてください" >
+echo"自然数を入れてください" > ${ans}
+./saidai.sh $i=a > ${result}
+diff ${ans} ${result} || exit 1
 #0が入力された場合
-echo "0の最大公約数はありません" >
+echo "0の最大公約数はありません" > ${ans}
+./saidai.sh $i=0 >${result}
+diff ${ans} ${result} || exit 1
+
+rm /tmp/$$-*
+
