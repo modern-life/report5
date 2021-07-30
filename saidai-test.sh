0eb68bcd57f90b63c33bc04429201226594e01bc
Ayaka Otsuka <z21073ao@aiit.ac.jp>
2021-07-30 22:42:28 +0900

    update saidai.sh

--- saidai-test.sh
+++ saidai-test.sh
@@ -5,31 +5,35 @@ result="/tmp/$$-result"
 err="/tmp/$$-error"
 
 
+#引数の数が足りない場合
+echo "文字を2つ入力してください" >${ans}
+./saidai.sh 1 > ${result}
+diff ${ans} ${result} || echo "error in 1-1" >> ${err}
 
 #1つ目に入力された文字が自然数でなかった場合
 
 echo "1つ目に入力した文字に問題があります 自然数を入れてください" > ${ans}
 ./saidai.sh a 10 > ${result}
-diff ${ans} ${result} || echo "error in 1-1"  >> ${err}
+diff ${ans} ${result} || echo "error in 2-1"  >> ${err}
 
 #1つ目に入力されたが文字が0だった場合
 
 echo "1つ目に入力した文字に問題があります 0の最大公約数はありません" > ${ans}
 ./saidai.sh 0 9 > ${result}
-diff ${ans} ${result} || echo "error in 1-2"  >> ${err}
+diff ${ans} ${result} || echo "error in 2-2"  >> ${err}
 
 
 #2つ目に入力された文字が自然数でなかった場合
 
 echo "2つ目に入力した文字に問題があります 自然数を入れてください" > ${ans}
 ./saidai.sh 20 b > ${result}
-diff ${ans} ${result} || echo "error in 2-1"  >> ${err}
+diff ${ans} ${result} || echo "error in 3-1"  >> ${err}
 
 #2つ目に入力された文字が0だった場合
 
 echo "2つ目に入力した文字に問題があります 0の最大公約数はありません" > ${ans}
 ./saidai.sh 5 0  > ${result}
-diff ${ans} ${result} || echo "error in 2-2"  >> ${err}
+diff ${ans} ${result} || echo "error in 3-2"  >> ${err}
 
 #2つの文字がどちらも自然数だった場合-最大公約数判定
 
